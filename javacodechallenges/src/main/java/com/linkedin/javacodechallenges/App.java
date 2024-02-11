089cda064c6a89524851be1f67c362c6b30b8d4e
Jayanth Seshadri <jay.seshadri@gmail.com>
2024-02-11 19:15:04 +0000

    01_04:  Introduce yourself

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,9 +1,9 @@
 package com.linkedin.javacodechallenges;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        
+public class App {
+    public static void main(String[] args) {
+        Person person1 = new Person("laksh", "vudu");
+        person1.setAge(34);
+        System.out.println(person1.introduceYourself());
     }
 }
--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
@@ -1,3 +1,25 @@
+package com.linkedin.javacodechallenges;
+
 public class Person {
-  
+  String firstName;
+  String lastName;
+  int age;
+
+  public Person(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  public int getAge() {
+    return this.age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String introduceYourself() {
+    return "Hi, I am " + firstName + "," + lastName + ". I am " + age + " years old.";
+  }
+
 }
